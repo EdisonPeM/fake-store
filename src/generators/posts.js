c1e9f73318835cece718e562d9ae33baf4f9943b
Edison Peñuela <edisonpe961206@hotmail.com>
2020-11-05 22:07:20 -0500

    adding posts

---
+++ src/generators/posts.js
@@ -0,0 +1,38 @@
+const faker = require('faker/locale/es');
+
+function createPost() {
+  const postName = faker.lorem.slug().split('-').join(' ');
+  return {
+    id: postName,
+    title: postName.split('-').join(' '),
+    content:
+      faker.lorem.sentences() +
+      '/n' +
+      faker.lorem.paragraphs(5) +
+      '/n' +
+      faker.lorem.words() +
+      '/n' +
+      faker.lorem.sentences() +
+      '/n' +
+      faker.lorem.paragraphs(3) +
+      '/n' +
+      faker.lorem.words() +
+      '/n' +
+      faker.lorem.sentences() +
+      '/n' +
+      faker.lorem.paragraphs(4),
+  };
+}
+
+function createPostList(length = 10) {
+  const pList = [];
+  for (let i = 0; i < length; i++) {
+    pList.push(createPost());
+  }
+  return pList;
+}
+
+module.exports = {
+  createPost,
+  createPostList,
+};
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const productsGenerator = require('./generators/products');
 const companiesGenerator = require('./generators/companies');
 const vehiclesGenerator = require('./generators/vehicles');
 const clientsGenerator = require('./generators/clients');
+const postsGenerator = require('./generators/posts');
 
 const generatedData = {
   users: usersGenerator.createUserList(4),
@@ -12,6 +13,7 @@ const generatedData = {
   companies: companiesGenerator.createCompanyList(10),
   vehicles: vehiclesGenerator.createVehicleList(50),
   clients: clientsGenerator.createClientList(20),
+  posts: postsGenerator.createPostList(7),
 };
 
 fs.writeFileSync('db.json', JSON.stringify(generatedData, null, '\t'));
